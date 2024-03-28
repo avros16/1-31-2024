b5cb71ce0fd3755a13c5142d5c9a04c34b7242a3
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-28 16:12:02 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@1-31-2024/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  std::cout << "Hello Student!\n";
+  std::cout << "Hello Class!\n";
+  std::cout << "Hello This is the last line!\n";
+  return 0;
+}
+
+
+// cin >> - use when user is inputting information
+//cout << - use when machine is giving info to user 
+// the arrows matter. end lines with semi colons
+// the \n (called escape sequences) ends the line and prints the next line on a new line. goes inside the "". it is not necessary 
+// \t is tab, gives space on same line, not differet line 
+// <<std:endl also ends line. good for variables 
+// /* .... */ for long paragraph comments, like this one
+// https://unicode.org/emoji/charts/full-emoji-list.html. use forward slash for emojis and include 3 0'z where the plus is.
